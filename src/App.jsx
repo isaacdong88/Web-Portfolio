973972116766b193f5fda121e21848c009b5b23a
isaacdong88 <isaacdong88@gmail.com>
2023-05-09 12:57:07 -0400

    add components, update app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter } from "react-router-dom";
+import About from "./components/About";
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+
 const App = () => {
-
   return (
-    <div>
-      3d Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <NavBar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Projects />
+      </div>
+    </BrowserRouter>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
